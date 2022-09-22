d178fe6d50f34a23d174cd971540da7c7ca45d57
Carlos Retamales <carlos.retamales.a@usach.cl>
2022-09-22 04:38:38 -0300

    [Carlos Retamales] - Justificativos

--- MSapp/src/main/java/cl/MSapp/controllers/JustificativosController.java
+++ MSapp/src/main/java/cl/MSapp/controllers/JustificativosController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,12 +23,20 @@ public class JustificativosController {
     @GetMapping
     public String listAllJustificativos(Model model){
         List<Justificativos> justificativos = justificativosService.listAllJustificativos();
+        /*
         if(justificativos.isEmpty()){
             ResponseEntity.noContent().build();
             return "justificativos";
-        }
+        }*/
         ResponseEntity.ok(justificativos);
         model.addAttribute("justificativos", justificativos);
+        model.addAttribute("justificativo",new Justificativos());
         return "justificativos";
+    }
+
+    @PostMapping
+    public String createJustificativos(Model model, @ModelAttribute Justificativos justificativo){
+        Justificativos justificativoNuevo = justificativosService.createJustificativos(justificativo);
+        return "redirect:/justificativos/";
     }
 }
