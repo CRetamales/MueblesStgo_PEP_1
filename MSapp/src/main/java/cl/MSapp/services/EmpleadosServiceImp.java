6a162e2b271b3ca0a8a8252d0e505f5d782d2013
Carlos Retamales <carlos.retamales.a@usach.cl>
2022-10-04 07:28:25 -0300

    [Carlos Retamales] - arreglo reporte

--- MSapp/src/main/java/cl/MSapp/services/EmpleadosServiceImp.java
+++ MSapp/src/main/java/cl/MSapp/services/EmpleadosServiceImp.java
@@ -2,6 +2,8 @@ package cl.MSapp.services;
 
 import cl.MSapp.entities.Empleados;
 import cl.MSapp.repositories.EmpleadosRepository;
+import cl.MSapp.entities.Reportes;
+import cl.MSapp.repositories.ReportesRepository;
 import cl.MSapp.Complementarias;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,9 @@ public class EmpleadosServiceImp implements EmpleadosService {
     @Autowired
     private EmpleadosRepository empleadosRepository;
 
+    @Autowired
+    private ReportesRepository reportesRepository;
+
     @Override
     public List<Empleados> listAllEmpleados() {
         return empleadosRepository.findAll();
@@ -28,6 +33,20 @@ public class EmpleadosServiceImp implements EmpleadosService {
 
     @Override
     public Empleados createEmpleados(Empleados empleados) {
+        Reportes reporte = new Reportes();
+        reporte.setRut(empleados.getRut());
+        reporte.setNombre_empleado("a");
+        reporte.setCotizacion_salud(1);
+        reporte.setCategoria("C");
+        reporte.setDescuentos(1);
+        reporte.setSueldo_bruto(1);
+        reporte.setCotizacion_previsional(1);
+        reporte.setSueldo_fijo(1);
+        reporte.setBonificacion_anho(1);
+        reporte.setPago_horas_extras(1);
+        reporte.setAnho_empresa(1);
+        reporte.setSueldo_final(1);
+        reportesRepository.save(reporte);
         return empleadosRepository.save(empleados);
     }
 
